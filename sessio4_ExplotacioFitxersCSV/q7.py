e876f45b2d1240c3c6775ff917584aa23873d27a
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2021-11-04 21:09:18 +0100

    Add files via upload

--- sessio4_ExplotacioFitxersCSV/q7.py
+++ sessio4_ExplotacioFitxersCSV/q7.py
@@ -25,6 +25,12 @@ def sort_ranking(ranking: dict[str, int]) -> list[tuple[str, int]]:
 
     ranking_items:  list[tuple[str, int]] = list(ranking.items())
     get_num:        function              = lambda item: item[1]
+    # una función que devuelve un valor sencillo que sorted sabe ordenar (basico lo ordena
+    # fàcil, int,string,double), en este caso recibimos la tupla, y devolvemos solo el 
+    # segundo valor de la tupla, o sea el numero de ocurrencias
+    # si pusieramos solo sorted(ranking_items) ordenaria los strings,
+    # con key_get_num me devuelve el numero de la tupla y es por allí donde ordena
+    # reverse=true orden descendente
     sorted_ranking: list[tuple[str, int]] = sorted(ranking_items, key=get_num, reverse=True)
 
     return sorted_ranking
@@ -42,7 +48,7 @@ def q7():
     # Make ranking
     category_ranking: dict[str, int] = {category: get_num_entries(category, clean_entries) for category in unique_categories_list}
 
-    pprint(category_ranking)
+    #pprint(category_ranking)
     # Sort ranking
     sorted_category_ranking: list[tuple[str, int]] = sort_ranking(category_ranking)
 
