d5b93c8150a8ad04155cb94ef46724b7ec827dcb
Miquel Angel Bardají <miki@example.com>
2021-12-16 20:59:37 +0100

    q8

--- Sessió12_ScimagoPandas/q8.py
+++ Sessió12_ScimagoPandas/q8.py
@@ -29,14 +29,20 @@ def q8():
     # Get sport categories (3 categories)
     sport_mask:       pd.Series = unique_categories.str.contains('Sport')
     sport_categories: pd.Series = unique_categories[sport_mask]
-
+    print("sport categories")
+    print(sport_categories)
+    
     # Get ids (ranks) in sport-related categories and remove duplicates.
     sport_ids_mask: pd.Series =  categories.loc[:, 'Category'].isin(sport_categories)
+
+    print("sport sport_ids_mask")
+    print(sport_ids_mask)
     sport_ids:      pd.Series = (categories.loc[sport_ids_mask, 'Rank']
                                            .drop_duplicates()
                                            .reset_index(drop=True)
     )
-
+    print("sport_ids")
+    print(sport_ids)
     # Merge back to get the whole entries
     sport_entries: pd.DataFrame = pd.merge(sport_ids, entries, on='Rank')
 
